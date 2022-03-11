34e87c021869d1da56e8c48557ec6b068248cb21
mpaul902 <matthewjpaul2003@gmail.com>
2022-03-11 11:41:10 -0500

    Update testMotor.py

--- MotorTesting/testMotor.py
+++ MotorTesting/testMotor.py
@@ -23,7 +23,9 @@ duty = 2
 # Loop for duty values from 2 to 12 (0 to 180 degrees)
 while duty <= 12:
     servo1.ChangeDutyCycle(duty)
-    time.sleep(1)
+    time.sleep(0.3)
+    servo1.ChangeDutyCycle(0)
+    time.sleep(0.7)
     duty = duty + 1
 
 # Wait a couple of seconds
@@ -32,7 +34,9 @@ time.sleep(2)
 # Turn back to 90 degrees
 print ("Turning back to 90 degrees for 2 seconds")
 servo1.ChangeDutyCycle(7)
-time.sleep(2)
+time.sleep(0.5)
+servo1.ChangeDutyCycle(0)
+time.sleep(1.5)
 
 #turn back to 0 degrees
 print ("Turning back to 0 degrees")
